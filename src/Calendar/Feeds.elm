7330339c8fd2c9cdd09449a732452a1f7d4d8227
Yu Onaga <ursus.cauda.elongata@gmail.com>
2022-05-05 11:21:41 +0900

    Retrieve and commit YouTube feeds automatically
    It is kind of a workaround rather than a solution, but it should work
    for the time being.

--- src/Calendar/Feeds.elm
+++ src/Calendar/Feeds.elm
@@ -14,31 +14,31 @@ type alias Feed =
 preset : Dict String Feed
 preset =
     Dict.fromList
-        [ ( "https://www.youtube.com/feeds/videos.xml?channel_id=UCEcMIuGR8WO2TwL9XIpjKtw"
+        [ ( "UCEcMIuGR8WO2TwL9XIpjKtw.xml"
           , Feed "ケープペンギン / African Penguin"
                 "https://www.youtube.com/channel/UCEcMIuGR8WO2TwL9XIpjKtw"
                 "https://yt3.ggpht.com/ytc/AKedOLSiSzCCyj5TBipvNgNcz0NrPZbvJZmZQU9JUFE-=s60"
                 True
           )
-        , ( "https://www.youtube.com/feeds/videos.xml?channel_id=UCmYO-WfY7Tasry4D1YB4LJw"
+        , ( "UCmYO-WfY7Tasry4D1YB4LJw.xml"
           , Feed "フンボルトペンギン / Humboldt Penguin"
                 "https://www.youtube.com/channel/UCmYO-WfY7Tasry4D1YB4LJw"
                 "https://yt3.ggpht.com/ytc/AKedOLSr75ivVQI4bHcaoMOaYxPjbRnL3-2VCSNuHbJ-=s60"
                 True
           )
-        , ( "https://www.youtube.com/feeds/videos.xml?channel_id=UCMpw36mXEu3SLsqdrJxUKNA"
+        , ( "UCMpw36mXEu3SLsqdrJxUKNA.xml"
           , Feed "シマハイイロギツネ / Island Fox"
                 "https://www.youtube.com/channel/UCMpw36mXEu3SLsqdrJxUKNA"
                 "https://yt3.ggpht.com/2ohbFqFqLbEw66rWMhTjb-wpa5X9APonb1KZiiBJbmGcS69yKUwtmLSHfhPUSF4snFp1O9r_=s60"
                 True
           )
-        , ( "https://www.youtube.com/feeds/videos.xml?channel_id=UCabMjG8p6G5xLkPJgEoTnDg"
+        , ( "UCabMjG8p6G5xLkPJgEoTnDg.xml"
           , Feed "コヨーテ / Coyote"
                 "https://www.youtube.com/channel/UCabMjG8p6G5xLkPJgEoTnDg"
                 "https://yt3.ggpht.com/VwLc2w11lh_JlrsDB9P4OvBJpqaoAZdS08gqQx7vtJ5-4DjsEiP5Un6xmT0q8VE6zr8uXYEnTqg=s60"
                 True
           )
-        , ( "https://www.youtube.com/feeds/videos.xml?channel_id=UCdNBhcAohYjXlUVYsz8X2KQ"
+        , ( "UCdNBhcAohYjXlUVYsz8X2KQ.xml"
           , Feed "ダイアウルフ / Dire Wolf"
                 "https://www.youtube.com/channel/UCdNBhcAohYjXlUVYsz8X2KQ"
                 "https://yt3.ggpht.com/yJUytRAl-MwBrGmIXMdctRgcNuAborghz1SGt2o6KDewrB1aP6saZLdX9HWBPdF5JEutZ6aBKNY=s60"
