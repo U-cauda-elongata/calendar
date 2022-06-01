1eb19296228f82789fdc53f58d2f3629e1cd043d
Yu Onaga <ursus.cauda.elongata@gmail.com>
2022-06-02 02:12:49 +0900

    refactor: init app in `app.js`

---
+++ src/init.js
@@ -0,0 +1,56 @@
+(() => {
+	const app = Elm.Main.init({
+		flags: {
+			features: {
+				copy: !!navigator.clipboard,
+				share: !!navigator.share,
+			},
+			languages: navigator.languages,
+		},
+	});
+
+	app.ports.setLang.subscribe(lang => {
+		const h = document.documentElement;
+		h.lang = lang;
+		h.classList.add('lang-confirmed');
+	});
+
+	app.ports.slideViewportInto.subscribe(id => {
+		const node = document.getElementById(id);
+		if (node) {
+			requestAnimationFrame(() => {
+				node.scrollIntoView({ behavior: 'smooth', block: 'center' });
+			});
+		}
+	});
+
+	app.ports.showModal.subscribe(id => {
+		const node = document.getElementById(id);
+		if (node) {
+			if (node.showModal) {
+				node.showModal();
+			} else {
+				node.toggleAttribute('open', true);
+			}
+		}
+	});
+
+	app.ports.close.subscribe(id => {
+		const node = document.getElementById(id);
+		if (node) {
+			if (node.close) {
+				node.close();
+			} else {
+				node.toggleAttribute('open', false);
+			}
+		}
+	});
+
+	app.ports.copy.subscribe(text => {
+		navigator.clipboard.writeText(text);
+	});
+
+	app.ports.share.subscribe(data => {
+		navigator.share(data).catch(() => {})
+	});
+})();
