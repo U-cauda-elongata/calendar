9d5b8f8c11bbb21263b9539627f3fad0a8408d7f
Yu Onaga <ursus.cauda.elongata@gmail.com>
2022-06-19 09:38:58 +0900

    feat(observances): update observances

--- src/observances.js
+++ src/observances.js
@@ -1,64 +1,90 @@
 const observances = [
 	{
+		// <https://www.youtube.com/watch?v=Gxia9tP6jWM>
 		key: "African Penguin and Humboldt Penguin's debut anniversary",
 		month: 3,
 		date: 26,
 		sinceYear: 2021,
 	},
 	{
+		// <https://www.youtube.com/watch?v=MGYIVKqHW-Q>
 		key: "Island fox's debut anniversary",
 		month: 5,
 		date: 11,
 		sinceYear: 2021,
 	},
 	{
+		// <https://www.youtube.com/watch?v=vYx4QBSQnZ8>
 		key: "Coyote's debut anniversary",
 		month: 11,
 		date: 5,
 		sinceYear: 2021,
 	},
 	{
+		// <https://www.youtube.com/watch?v=Xb9qg_122yg>
 		key: "Dire Wolf's debut anniversary",
 		month: 2,
 		date: 19,
 		sinceYear: 2022,
 	},
 	{
+		// <https://www.youtube.com/watch?v=5iY_mXCBOPs>
 		key: "Caracal's debut anniversary",
 		month: 4,
 		date: 28,
 		sinceYear: 2022,
 	},
 	{
+		// <https://www.worldanimalday.org.uk/>
 		key: "World Animal Day",
 		month: 9,
 		date: 4,
+		// <https://www.worldanimalday.org.uk/img/resource/OriginofWorldAnimalDay-update2020-12.pdf>
+		sinceYear: 1929,
 	},
 	{
+		// <https://www.wildlifeday.org/>
 		key: "World Wildlife Day",
 		month: 2,
 		date: 3,
+		sinceYear: 2014,
 	},
 	{
+		// <https://www.un.org/en/observances/biological-diversity-day>
+		// <https://www.cbd.int/biodiversity-day>
 		key: "International Day for Biological Diversity",
 		month: 4,
 		date: 22,
+		sinceYear: 2001,
 	},
 	{
+		// <https://www.earthday.org/>
+		key: "Earth Day",
+		month: 3,
+		date: 22,
+		sinceYear: 1970,
+	},
+	{
+		// XXX: Is there a reliable materials on the day?
+		// Ideally, ones from McMurdo Station itself or NSF would be great.
 		key: "World Penguin Day",
 		month: 3,
 		date: 25,
 	},
 	{
+		// <https://www.worldmigratorybirdday.org/>
 		key: "World Migratory Bird Day",
 		month: 4,
 		week: 1,
 		day: 6,
+		// <https://www.worldmigratorybirdday.org/history>
+		sinceYear: 2018,
 	},
 	{
 		key: "World Migratory Bird Day",
 		month: 9,
 		week: 1,
 		day: 6,
+		sinceYear: 2018,
 	},
 ];
