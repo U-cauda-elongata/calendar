62b822303a3f659fcb6219b904bdcd313973912f
Yu Onaga <ursus.cauda.elongata@gmail.com>
2022-06-26 16:08:05 +0900

    feat(pagenate.rb): group collab streams in backend

--- scripts/paginate.rb
+++ scripts/paginate.rb
@@ -5,6 +5,7 @@ PAGE_SIZE = 50
 
 require 'fileutils'
 require 'json'
+require 'set'
 
 (meta, entries) = Dir.glob('data/*.json').each_with_object([[], []]) do |file, (meta, entries)|
   feed = open(file) do |f|
@@ -25,8 +26,13 @@ require 'json'
     entry['feed'] = feed_id
     entry['name'] = e['name']
     entry['live'] = e['live'] if e['live']
-    entry['upcoming'] = e['upcoming'] if e['upcoming']
-    entry['time'] = e['time']
+    entry['scheduledTime'] = e['scheduledTime'] if e['scheduledTime']
+    if e['time']
+      entry['time'] = e['time']
+    else
+      entry['upcoming'] = true
+      entry['time'] = e['scheduledTime']
+    end
     entry['duration'] = e['duration'] if e['duration']
     entry['link'] = e['link'] if e['link']
     entry['thumbnail'] = e['thumbnail'] if e['thumbnail']
@@ -39,6 +45,7 @@ title_to_id = meta.map do |m|
   [m['title'], m['id']]
 end.to_h
 
+# Detect collab members by `@`-mentions in the description.
 entries.each do |entry|
   description = entry.delete('description')
   members = description.each_line.filter_map do |line|
@@ -53,23 +60,96 @@ entries.each do |entry|
   end
 end
 
-entries.sort_by! do |e|
-  e['time']
+# Group multiple streams corresponding to a single collab.
+# Here a collab is defined as a set of streams that are/were scheduled for a same time, referring to
+# each other with `@`-mentions.
+entries.group_by do |entry|
+  entry.delete('scheduledTime')
+end.each_pair do |time, simul_entries|
+  next if not time or simul_entries.length <= 1
+
+  # Factorize the streams into weakly connected components in a graph of `@`-mentions:
+  graph = simul_entries.map do |entry|
+    [entry['feed'], Set.new(entry['members'])]
+  end.to_h
+
+  # Convert the graph into an undirected graph.
+  graph.each do |vertice, neighbors|
+    neighbors.each do |n|
+      graph[n].add(vertice)
+    end
+  end
+
+  # Get the connected components by a breadth-first search.
+  vertices = Set.new(graph.keys)
+  components = []
+  while start = vertices.each.first
+    component = Set.new
+    queue = [start]
+    until queue.empty?
+      vertice = queue.shift
+      component.add(vertice)
+      vertices.delete(vertice)
+      graph[vertice].each do |neighbor|
+        queue.push(neighbor) if vertices.include?(neighbor)
+      end
+    end
+    components.push(component)
+  end
+
+  components.each do |collab_members|
+    collab_entries = collab_members.flat_map do |feed|
+      simul_entries.select do |entry|
+        entry['feed'] == feed
+      end
+    end
+    if collab_entries.length > 1
+      collab_entries[1..].each do |entry|
+        entries.delete(entry)
+      end
+      entries[entries.find_index(collab_entries.first)] = collab_entries
+      collab_entries.sort_by! do |entry|
+        -entry['time']
+      end
+    end
+  end
 end
 
-pages = entries.each_slice(PAGE_SIZE).to_a
-if pages[-2] and pages[-1].length < PAGE_SIZE
-  latest = pages.pop
-  pages[-1].concat(latest)
+entries.sort_by! do |entry_or_collab|
+  -if entry_or_collab.is_a?(Array)
+    entry_or_collab.last['time']
+  else
+    entry_or_collab['time']
+  end
 end
-pages.each(&:reverse!)
-pages.reverse!
+
+# Like `Enumerable#each_slice(PAGE_SIZE).to_a`, but splits by the size of flattened page instead.
+pages = [[]]
+entries.reverse_each do |entry_or_collab|
+  prepend_to_existing = if entry_or_collab.is_a?(Array)
+    # Allow excess of up to half the length to make the page sizes around `PAGE_SIZE` by average.
+    pages.first.flatten.length + entry_or_collab.length / 2 <= PAGE_SIZE
+  else
+    pages.first.length + 1 <= PAGE_SIZE
+  end
+  if prepend_to_existing
+    pages.first.unshift(entry_or_collab)
+  else
+    pages.unshift([entry_or_collab])
+  end
+end
+
+if pages[1] and pages[0].flatten.length < PAGE_SIZE
+  # Make sure that the first page contains at least `PAGE_SIZE`.
+  latest = pages.shift
+  pages[0].unshift(*latest)
+end
 
 FileUtils.mkdir_p('feed')
-page = pages.shift || {}
+page = pages.shift
 file = 'latest.json'
 while page
-  STDERR.puts "Writing #{page.length} entries to `#{file}`..."
+  STDERR.puts "Writing #{page.flatten.length} entries to `#{file}`..."
   open("feed/#{file}", 'w') do |out|
     json = {}
     if meta
@@ -77,7 +157,7 @@ while page
       meta = nil
     end
     json['next'] = file = "#{pages.length}.json" unless pages.empty?
-    json['entries'] = page
+    json['entry_groups'] = page
     JSON.dump(json, out)
     page = pages.shift
   end
--- src/Feed.elm
+++ src/Feed.elm
@@ -30,7 +30,12 @@ decoder : D.Decoder Feed
 decoder =
     D.map3 Feed
         (D.oneOf [ D.field "meta" (D.list metaDecoder), D.succeed [] ])
-        (D.field "entries" <| D.list Event.decoder)
+        -- TODO: Implement some fancy representations for collab streams.
+        (D.field "entry_groups"
+            (D.list (D.oneOf [ Event.decoder |> D.map List.singleton, D.list Event.decoder ])
+                |> D.map List.concat
+            )
+        )
         (D.maybe <| D.field "next" D.string)
 
 
