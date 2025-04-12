ca582b197876f585f118cec759534330253f9993
Yu Onaga <ursus.cauda.elongata@gmail.com>
2025-04-12 16:13:05 +0900

    chore(feeds): update icon URLs
    We are now using images from the official website, as images from
    YouTube break once in a while.
    
    Because how closely the images on the official site resemble the icons
    from the corresponding YouTube channel varies from page to page, we are
    using different source pages on an ad-hoc basis.

--- src/feeds.ts
+++ src/feeds.ts
@@ -3,78 +3,89 @@ export default [
 		id: "yt:channel:UCEOugXOAfa-HRmRjKbH8z3Q",
 		title: "けものフレンズプロジェクト公式",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/qXk8L8kI7-cGKXCRRdEy3Td9fOZ7bjxg5AxX7Zc3zxzHQXy9d8PjcFzM8p_JOOnBu7Ay0Zdy5g=s60",
+		icon: "https://yt3.googleusercontent.com/OGpHZpsyaBemVIwSMawE19xLEqCtSM238ICAy9fChcziKHXDFQ37o6v9siYiHFaeuVxkaeAY=s60",
 	},
 	{
 		id: "yt:channel:UCrMKWvQuyFL1dckG2RLInDQ",
 		title: "パークスタッフ / Park Staff",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/phX9Lg0o-onftJgEhuwDMqSEP5CUbTu0nBlVnPF9HqF114s6k1Yd411zuWz8gscqWByzDmmYhQ=s60",
+		icon: "https://yt3.googleusercontent.com/m5n2IDiK9Sgea1W9ebBfWWd8VNu_6Vkx2Ck0y5MHpkg3SEEz7zPnByH1wiLGYe1czNvxrPWHaQ=s60",
 	},
 	{
 		id: "yt:channel:UCmYO-WfY7Tasry4D1YB4LJw",
 		title: "フンボルトペンギン / Humboldt Penguin",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/ytc/AIdro_nucgOj565yWEa2KXJsE7zzQrjhYG0h6bphYbUzSx7p1Q=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/4ea86e_1d7c4be5d5684dbc860824ad2a7ffcc0~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCMpw36mXEu3SLsqdrJxUKNA",
 		title: "シマハイイロギツネ / Island Fox",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/2ohbFqFqLbEw66rWMhTjb-wpa5X9APonb1KZiiBJbmGcS69yKUwtmLSHfhPUSF4snFp1O9r_=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/4ea86e_b3f690db598a49cf8b23ee6292bc7655~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCabMjG8p6G5xLkPJgEoTnDg",
 		title: "コヨーテ / Coyote",
 		lang: "en",
-		icon: "https://yt3.googleusercontent.com/-jp0xO_hk95EgFm1z4_EZOpXJRM0B8auEwifKeSEHZx09OHly9ygQHHIk3TTkuMZ6zLhhk0hXA=s60",
+		// https://www.kemov-project.com/%E3%82%B3%E3%83%A8%E3%83%BC%E3%83%86
+		icon: "https://static.wixstatic.com/media/91d475_164f6bf8bf5640d5a48e2f061bba6556~mv2.png/v1/fill/w_60,h_74/.png",
 	},
 	{
 		id: "yt:channel:UCdNBhcAohYjXlUVYsz8X2KQ",
 		title: "ダイアウルフ / Dire Wolf",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/B_QAdGj-_LGHj7rmJdLxVnyc4DvznyqnpEfnbstrExQA_eS7alt4v1FoM1e7HpEdq65QdRj8Coo=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/91d475_0396423b4f3a4490ad8c1c5de53b07c8~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCxm7yNjJsSvyvcG96-Cvmpw",
 		title: "カラカル / Caracal",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/6H0SlGWB0fpx3YR3wMkYaG9QQsEhkFUgcx4DodT-_cDEXLdEHpqfxg6XK5U-qJXrkioWTI9YDg=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/91d475_b99c62b492ce40e3941753cd66b006d6~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCNObi6xvj6QeZ0g7BhAbF7w",
 		title: "ジェネット / Large-spotted Genet",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/F6Ohg12s3EWoWndeHE75L9t6SSCUz3fldo5eXuvWKgnAIp6cJtBgKu2bcjLUEo2tdzO4hlIy=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/91d475_9cfb3db9c00f49ca86f410f167e41b8d~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCYa58DdXGAGMJQHqTxi-isA",
 		title: "ジョフロイネコ / Geoffroy’s Cat",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/9euqc6PdKxTfTJTOTDvuNU9R6zY9Vwjaj8IB3PaD0U7VtX_aMB9-w06t3vTaCH_onX9Rj954vQ=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/91d475_1608f379f61145cc814fcc8de562e728~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCnyE-wD1pE2GZOxA6OHjW9g",
 		title: "ウサギコウモリ / Brown Long-eared Bat",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/lOupphnIz6eL4UuzmA8GkM_n5vZdyZkd-MQ2PiUY4BhMKIZUySS_jFPcwvehVLhOKHmQ8CG1=s60",
+		// https://www.kemov-project.com/%E3%82%A6%E3%82%B5%E3%82%AE%E3%82%B3%E3%82%A6%E3%83%A2%E3%83%AA
+		icon: "https://static.wixstatic.com/media/91d475_b8c81443a64a4d7a8fb723bfd497bc0c~mv2.png/v1/fill/w_60,h_74/.png",
 	},
 	{
 		id: "yt:channel:UCtJSUW-5FnwfaivXpluABWA",
 		title: "ジャングルキャット / Jungle Cat",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/DoY1-B-GiXKzKMFVyqDs61vkLuoaruCs-TjJKtThdX18Hcqpq29HyqI8SWTDafLTJLZQCRoSlQ=s60",
+		// https://www.kemov-project.com/
+		icon: "https://static.wixstatic.com/media/91d475_3922b63b31904dc585952d4cec0a9649~mv2.png/v1/fill/w_60,h_60/.png",
 	},
 	{
 		id: "yt:channel:UCKob71cjOlyYF5bgvtGuNKQ",
 		title: "シマリス / Siberian Chipmunk",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/AEo8xi-6_WDxJt3s0Zd-5YcibmtziIxJ1Nkx_pJATh4yDj3Q0I-_RZl26klky-JIeKUZIhp3VQ=s60",
+		// https://www.kemov-project.com/%E3%82%B7%E3%83%9E%E3%83%AA%E3%82%B9
+		icon: "https://static.wixstatic.com/media/91d475_440fca1bebe04eb88c73929a3d34abbf~mv2.png/v1/fill/w_60,h_74/.png",
 	},
 	{
 		id: "yt:channel:UCEcMIuGR8WO2TwL9XIpjKtw",
 		title: "ケープペンギン / African Penguin",
 		lang: "ja",
-		icon: "https://yt3.googleusercontent.com/ytc/AIdro_lkvVhwNCvG354xLsHirSmluolfqcyJrz0MkG6IVtywBQ=s60",
+		// https://www.kemov-project.com/%E3%82%B1%E3%83%BC%E3%83%97%E3%83%9A%E3%83%B3%E3%82%AE%E3%83%B3
+		icon: "https://static.wixstatic.com/media/4ea86e_c67fa2af584343bb8548315d9e377529~mv2.png/v1/fill/w_60,h_74/.png",
 	},
 ];
